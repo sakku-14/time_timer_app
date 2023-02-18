f3273d58b867f30129acdd428f2bec77ef238eab
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2023-02-18 12:13:40 +0900

    不要な実装の削除

--- lib/View/time_page_view.dart
+++ lib/View/time_page_view.dart
@@ -12,14 +12,6 @@ class TimePageView extends StatefulWidget {
 }
 
 class _TimePageViewState extends State<TimePageView> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,51 +20,6 @@ class _TimePageViewState extends State<TimePageView> {
       ),
       body: const TimerBody(),
       bottomNavigationBar: const TimePageBottomNavigationBar(),
-    );
-  }
-}
-
-class PlusFloatingActionButton extends StatelessWidget {
-  const PlusFloatingActionButton({Key? key, required this.incrementCounter})
-      : super(key: key);
-  final Function incrementCounter;
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () => incrementCounter(),
-      tooltip: 'Increment',
-      child: const Icon(Icons.add),
-    );
-  }
-}
-
-class DefaultTextWidget extends StatelessWidget {
-  const DefaultTextWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text(
-      'You have pushed the button this many times:',
-    );
-  }
-}
-
-class CountWidget extends StatelessWidget {
-  const CountWidget({
-    super.key,
-    required int counter,
-  }) : _counter = counter;
-
-  final int _counter;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      '$_counter',
-      style: Theme.of(context).textTheme.headlineMedium,
     );
   }
 }
