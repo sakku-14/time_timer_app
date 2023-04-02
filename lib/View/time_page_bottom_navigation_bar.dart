9fffb4f15173327210eafbfeafe0a9f3a57d310a
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2023-04-02 21:08:49 +0900

    bottom_navigation_barの選択されたアイテムの色を指定

--- lib/View/time_page_bottom_navigation_bar.dart
+++ lib/View/time_page_bottom_navigation_bar.dart
@@ -14,6 +14,7 @@ class TimePageBottomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      selectedItemColor: Colors.red,
       currentIndex: currentIndex,
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
